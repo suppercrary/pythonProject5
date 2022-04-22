c9eaad06ba54ff267daaa1a486851a664e597425
danh <admin@rootframe.com>
2022-04-22 13:56:19 +0700

    Initial commit

--- test_one.py
+++ test_one.py
@@ -1,9 +1,11 @@
-def test_file2_method1 ():
-	x = 5
-	y = 6
-	assert x + 1 == y, "kiểm tra không thành công"
-	assert x == y, "kiểm tra không thành công vì x =" + str (x) + "y =" + str (y)
-def test_file2_method2 ():
-	x = 5
-	y = 6
-	assert x + 1 == y, "kiểm tra không thành công"
+def test_file2_method1():
+    x = 5
+    y = 6
+    assert x + 1 == y, "kiểm tra không thành công"
+    assert x == y, "kiểm tra không thành công vì x =" + str(x) + "y =" + str(y)
+
+
+def test_file2_method2():
+    x = 5
+    y = 6
+    assert x + 1 == y, "kiểm tra không thành công"
